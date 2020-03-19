17953c6ed4ef0ba9ffb029e1a4720e36be10bcb9
PaulaLukasiewicz <56148851+PaulaLukasiewicz@users.noreply.github.com>
2020-03-19 17:20:37 +0100

    Zastosowanie sugestii

--- test1.java
+++ test1.java
@@ -1,8 +1,9 @@
 public class Hello {
   public static void main(String[] args){
     System.out.print("Hello World");
-    a=2;
-    b=5;
-    c=a+b;
+    int a=2;
+    int b=5;
+    int c=a+b;
+    System.out.print(c);
   }
 }
